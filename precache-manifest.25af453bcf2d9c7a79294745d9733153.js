ab5bba4d7d7d5f3849254e65cf4c6b02dbc37eb0
Mac <mac@Macs-MacBook-Air.local>
2019-09-12 16:36:33 +0300

    Updates

---
+++ precache-manifest.25af453bcf2d9c7a79294745d9733153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1ab119ffa37f2113e26876323bf995",
+    "url": "/312/index.html"
+  },
+  {
+    "revision": "ce37c2bbd934edbe65bb",
+    "url": "/312/static/css/main.af063eef.chunk.css"
+  },
+  {
+    "revision": "dd39b7454669a1c5de43",
+    "url": "/312/static/js/2.7a648115.chunk.js"
+  },
+  {
+    "revision": "ce37c2bbd934edbe65bb",
+    "url": "/312/static/js/main.5eeef7c5.chunk.js"
+  },
+  {
+    "revision": "8c9abb248e0c32678ca4",
+    "url": "/312/static/js/runtime~main.5c4e0e9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/312/static/media/logo.5d5d9eef.svg"
+  }
+]);
